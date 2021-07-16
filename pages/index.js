7aeadcd8189485b80ac493968b4bf82cad5c9755
Hectordr03 <Hectordr03@gmail.com>
2021-07-16 13:34:35 -0400

    conditional rendering on btn click

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,25 @@ import Delay from '../components/delay';
 import React from 'react';
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showExcuse: false
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			showExcuse: false,
+		};
 
-  }
-	//TODO: When user clicks "generate" btn create random excuse using "intro", "scapegoat", and "delay" structure
+		this.handleClick = this.handleClick.bind(this);
+	}
+	//TODO: When user clicks "generate" btn create random excuse using "intro", "scapegoat", and "delay" structure ::DONE::
+
+	handleClick = () => {
+		let current = this.state.showExcuse;
+		this.setState({
+			showExcuse: !current,
+		});
+	};
+
+	//TODO: Excuse element should appear once and stay on screen as user generates new excuses
+
 	render() {
 		return (
 			<div className={styles.homeContainer}>
@@ -20,10 +31,14 @@ class Home extends React.Component {
 					Greetings! This is your random excuse generator! This tool
 					will help you get out of any situation
 				</h1>
-				<button>Generate!</button>
-				<Intro />
-				<Scapegoat />
-				<Delay />
+				<button onClick={this.handleClick}>Generate!</button>
+				{this.state.showExcuse === true ? (
+					<div className={styles.excuse}>
+						<Intro className={styles.child}/>
+						<Scapegoat className={styles.child}/>
+						<Delay className={styles.child}/>
+					</div>
+				) : null}
 			</div>
 		);
 	}
